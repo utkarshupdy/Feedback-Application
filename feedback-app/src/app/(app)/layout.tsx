35353fe18468f13dca36e5072fff5f89df6c1af4
Utkarsh Kumar Upadhyay <ukupadhyay6789@gmail.com>
2024-12-20 15:50:52 +0530

    file directory changed and 2 new routes created

--- feedback-app/src/app/(app)/layout.tsx
+++ feedback-app/src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 import AuthProvider from "@/context/Auth-Provider";
 import dbConnect from "@/lib/dbConnect";
 import { Toaster } from "@/components/ui/toaster";
