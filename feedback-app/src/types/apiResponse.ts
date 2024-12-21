e869e4475217c97364cbdb5864279d83ee1b08e4
Utkarsh Kumar Upadhyay <ukupadhyay6789@gmail.com>
2024-12-21 12:19:52 +0530

    user dashboard created

--- feedback-app/src/types/apiResponse.ts
+++ feedback-app/src/types/apiResponse.ts
@@ -1,10 +1,10 @@
-import { Message } from "postcss";
+import { Message } from "@/model/User";
 
 
 export interface ApiResponse{
     success: boolean;
     message: string;
     isAcceptingMessage?: boolean // means assign it as optional means it not necessary to be in api response , but if it exist , its type is this .....
-    messages?: Array<Message>
+    messages?: Message[];
 }
 
